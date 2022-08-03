7e207524c696851ce467b454acda6a509fc7ac4c
Jeffrey Jansma <me@perplexity.nl>
2022-08-03 15:21:23 +0200

    Add files via upload

---
+++ Get-AtalData.ps1
@@ -0,0 +1,38 @@
+[CmdletBinding()]
+param (
+    [Parameter(ValueFromPipeline, Position=0,Mandatory=$true)][string[]]$SensorIP
+)
+
+Begin 
+{
+    $SensorDataTable = @()
+}
+
+Process 
+{
+    Foreach($Sensor in $SensorIP)
+    {
+        if (-Not(Test-Connection -Ping -IPv4 $Sensor -Count 1 -Quiet))
+        {
+            Write-Error "The sensor $Sensor could be pinged to test the connection, please re(connect) the sensor and try again."
+            Continue
+        }
+      
+        $AtalSensorPath = "http://$Sensor"
+        $AtalValuePath = "/values.xml"
+
+        $Result =  (Invoke-RestMethod -Method Get -Uri ($AtalSensorPath + $AtalValuePath))
+        
+        $SensorDataTable += $Result.root
+
+        if (-Not($Result.root))
+        {
+            Write-Error "The request was not completed succesfully, please (re)connect the sensor $Sensor and try again."
+        }
+    }
+}
+
+End
+{
+    return $SensorDataTable
+}
